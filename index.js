7711939fd93c3dc39849a2b4b15d7e134a36602f
Bayramgeldi <byrammr@gmail.com>
2020-11-20 11:56:21 -0500

    added delete request

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const express = require('express');
-const nodemon = require('nodemon');
+const express = require("express");
+const nodemon = require("nodemon");
 const app = express();
-const path = require('path');
-const logger = require('./middleware/logger')
+const path = require("path");
+const logger = require("./middleware/logger");
+const expbhs = require("express-handlebars");
 //app.use(logger);
 
+//Handlebars Middleware
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //Set static folder
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.use('/api/members', require('./routes/api/members'));
+app.use("/api/members", require("./routes/api/members"));
 
 const PORT = process.env.PORT || 5000;
 
